697b7ad646be69d339828eb58b9ecf8b9f1812dd
Tamal-maker <135466336+Tamal-maker@users.noreply.github.com>
2024-08-08 12:27:39 +0530

    Add files via upload

---
+++ SQL PROJECTS/TIME SERIES ANALYSIS.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE TIME_SERIES_ANALYSIS;
+
+SELECT * FROM new_superstore
+
+/* 1) Use the LEAD window function to create a new column sales_next that displays the sales of the next row in the dataset. 
+This function will help you quickly compare a given row’s values and values in the next row.*/
+
+SELECT Product_ID, Category, Product_Name, Sales, LEAD(Sales) OVER(PARTITION BY CATEGORY ORDER BY SALES DESC)SALES_NEXT FROM new_superstore
+
+/* 2) Create a new column sales_previous to display the values of the row above a given row.*/
+
+SELECT Product_ID, Category, Product_Name, Sales, LAG(Sales) OVER(PARTITION BY CATEGORY ORDER BY SALES DESC)SALES_PREVIOUS FROM new_superstore
+
+/* 3) Rank the data based on sales in descending order using the RANK function*/
+
+SELECT *, RANK() OVER (ORDER BY sales DESC)[SALES RANK] FROM new_superstore 
+
+/* 4) Use common SQL commands and aggregate functions to show the monthly and daily sales averages.*/
+SELECT * FROM new_superstore
+
+/*MONTHLY SALES AVERAGE*/
+SELECT YEAR(Order_Date)Year, MONTH(Order_Date)Month, AVG(Sales) AS Monthly_Sales_Average FROM new_superstore GROUP BY YEAR(Order_Date), MONTH(Order_Date)ORDER BY Year, Month;
+
+/*DAILY SALES AVERAGE*/
+SELECT Order_Date, AVG(Sales) AS Daily_Sales_Average FROM new_superstore GROUP BY Order_Date ORDER BY Order_Date;
+
+/* 5) Analyze discounts on two consecutive days.*/    
+
+WITH CTE AS (SELECT Order_Date, Discount, LAG(Discount) OVER (ORDER BY Order_Date)Previous_Day_Discount FROM new_superstore) SELECT Order_Date, Discount AS Today_Discount, Previous_Day_Discount,    CASE        WHEN Discount > Previous_Day_Discount THEN 'Increase'        WHEN Discount < Previous_Day_Discount THEN 'Decrease'        ELSE 'No Discount'    END AS Discount_TrendFROM CTE;
+
+/* 6) Evaluate moving averages using the window functions.*/
+
+/*Default Window Size*/SELECT Order_Date, Sales, AVG(Sales) OVER (ORDER BY Order_Date) AS Moving_Average FROM new_superstore;     --windows analytical function/*Explicit Window Size*/SELECT Order_Date, Sales, AVG(Sales) OVER(ORDER BY ORDER_DATE ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)MOVING_AVG FROM new_superstore;   --windows frame/* 7) Calculate Running total using windows frame.*/SELECT Order_Date, Quantity, SUM(Sales) OVER(ORDER BY ORDER_DATE ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)RUNNING_TOTAL FROM new_superstore/* 8) Group the data based on the order of quantity & each group should have same number of rows.*/   SELECT Order_Date, Customer_ID, Sales, Quantity, NTILE(2000) OVER(ORDER BY QUANTITY)NTILE FROM new_superstore
